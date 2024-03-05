39dd2c9a0c41010a26f666131506a2c0bb25b8f1
Mikssxed <eksilonxd@gmail.com>
2024-03-05 19:38:23 +0100

    changed projects and skills

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,21 +1,20 @@
 import {
-  car,
+  ant,
   contact,
   css,
-  estate,
+  docker,
   git,
   github,
   html,
   javascript,
+  less,
   linkedin,
   mui,
-  pricewise,
+  pokeball,
   react,
   redux,
-  snapgram,
   summiz,
   tailwindcss,
-  threads,
   typescript,
 } from "../assets/icons";
 import { ulam } from "../assets/images";
@@ -29,6 +28,11 @@ export const skills = [
   {
     imageUrl: css,
     name: "CSS",
+    type: "Frontend",
+  },
+  {
+    imageUrl: less,
+    name: "less",
     type: "Frontend",
   },
   {
@@ -71,6 +75,16 @@ export const skills = [
     name: "Tailwind CSS",
     type: "Frontend",
   },
+  {
+    imageUrl: docker,
+    name: "Docker",
+    type: "Frontend",
+  },
+  {
+    imageUrl: ant,
+    name: "Ant Design",
+    type: "Frontend",
+  },
 ];
 
 export const experiences = [
@@ -109,51 +123,19 @@ export const socialLinks = [
 
 export const projects = [
   {
-    iconUrl: pricewise,
-    theme: "btn-back-red",
-    name: "Amazon Price Tracker",
-    description:
-      "Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.",
-    link: "https://github.com/adrianhajdin/pricewise",
-  },
-  {
-    iconUrl: threads,
-    theme: "btn-back-green",
-    name: "Full Stack Threads Clone",
-    description:
-      'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-    link: "https://github.com/adrianhajdin/threads",
-  },
-  {
-    iconUrl: car,
-    theme: "btn-back-blue",
-    name: "Car Finding App",
-    description:
-      "Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.",
-    link: "https://github.com/adrianhajdin/project_next13_car_showcase",
-  },
-  {
-    iconUrl: snapgram,
-    theme: "btn-back-pink",
-    name: "Full Stack Instagram Clone",
-    description:
-      "Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
-    link: "https://github.com/adrianhajdin/social_media_app",
-  },
-  {
-    iconUrl: estate,
-    theme: "btn-back-black",
-    name: "Real-Estate Application",
-    description:
-      "Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.",
-    link: "https://github.com/adrianhajdin/projects_realestate",
-  },
-  {
     iconUrl: summiz,
     theme: "btn-back-yellow",
     name: "AI Summarizer Application",
     description:
       "App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.",
-    link: "https://github.com/adrianhajdin/project_ai_summarizer",
+    link: "https://github.com/Mikssxed/ai-summarizer",
+  },
+  {
+    iconUrl: pokeball,
+    theme: "btn-back-pink",
+    name: "Pokemon Team Battler (IN PROGRESS)",
+    description:
+      "App that allows you to create pokemon team and test it against one of the strongest team",
+    link: "https://github.com/Mikssxed/pokemon-app",
   },
 ];
